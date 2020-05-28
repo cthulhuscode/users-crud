102efa0a2df107c463b50886632267ad44eacadd
Enrique <37712610+enrdevp@users.noreply.github.com>
2020-05-28 14:05:43 -0500

    database credentials deleted

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCQnyITTHKzQ0Yn9eASzHWU9D2-GtMjyb8',
-    authDomain: 'proyectousuarios-2b570.firebaseapp.com',
-    databaseURL: 'https://proyectousuarios-2b570.firebaseio.com',
-    projectId: 'proyectousuarios-2b570',
-    storageBucket: 'proyectousuarios-2b570.appspot.com',
-    messagingSenderId: '888272893966',
-    appId: '1:888272893966:web:06f73536916010efdfe45f',
-    measurementId: 'G-7EVZ96TBL0',
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
   },
 };
